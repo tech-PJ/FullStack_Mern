c89db346ef82fb038e3e43ff4c1ad2f4088e6900
tech-pj <prathamjiwanani445@gmail.com>
2024-06-30 18:31:11 +0530

    middleware,api  test, err handling done

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -11,6 +14,21 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 })
 const app=express();
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!')
 });
+
+app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode||500;
+    const msg=err.msg||"Invalid server Error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        msg
+    });
+})
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='p-3  max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4 '>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'/>
+        <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password'/>
+        <input type="text" placeholder='email' className='border p-3 rounded-lg' id='email'/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+      </form>
+    </div>
   )
 }
 
