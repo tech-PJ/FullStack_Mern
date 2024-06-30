7f0c636e618b1ba65714ceda30ad281a4226265d
tech-pj <prathamjiwanani445@gmail.com>
2024-06-30 16:13:49 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
+import Header from './components/Header'
 
 const App = () => {
   return (
   <BrowserRouter>
+  <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/sign-in' element={<SignIn/>}/>
